69cf1cd1e1f5276b697174772806c793e60e4d7f
Morgan Hein <work@morganhe.in>
2022-08-19 16:36:28 -0700

    feat: equipment timezones

--- db/migrations/000001_create_equipment_table.up.sql
+++ db/migrations/000001_create_equipment_table.up.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS equipment
    id               INTEGER  NOT NULL PRIMARY KEY
   ,customer         VARCHAR(100) NOT NULL
   ,fleet            VARCHAR(100) NOT NULL
-  ,equipment_id     INT  NOT NULL
+  ,equipment_id     VARCHAR(100)  NOT NULL
   ,equipment_status VARCHAR(1) NOT NULL
-  ,date_added       TIMESTAMP  NOT NULL
-  ,date_removed     TIMESTAMP
+  ,date_added       TIMESTAMPTZ  NOT NULL
+  ,date_removed     TIMESTAMPTZ
 );
