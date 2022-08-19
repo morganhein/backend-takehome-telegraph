461f7ad0ce211ea2890dc3cae54a283bc0edf461
Morgan Hein <work@morganhe.in>
2022-08-19 16:37:12 -0700

    feat: time package for csv handling

--- pkg/time/time.go
+++ pkg/time/time.go
@@ -1,1 +1,35 @@
 package time
+
+import "time"
+
+type DateTime struct {
+	time.Time
+}
+
+var format = "2006-01-02 15:04:05"
+
+func (date *DateTime) MarshalCSV() (string, error) {
+	return date.Time.Format(format), nil
+}
+
+func (date *DateTime) UnmarshalCSV(csv string) (err error) {
+	if csv == "" {
+		date.Time = time.Time{}
+		return nil
+	}
+	date.Time, err = time.Parse(format, csv)
+	return err
+}
+
+func (date *DateTime) MarshalJson() (string, error) {
+	return date.MarshalJson()
+}
+
+func (date *DateTime) UnmarshalJson(input []byte) (err error) {
+	if len(input) == 0 {
+		date.Time = time.Time{}
+		return nil
+	}
+	err = date.Time.UnmarshalJSON(input)
+	return err
+}
