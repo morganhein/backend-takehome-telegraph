2f0d4edd52e84e25639c9f8d63e47387174a923c
Morgan Hein <work@morganhe.in>
2022-08-19 17:02:37 -0700

    fix: from_mark_id fixed

--- db/migrations/000002_create_events_table.up.sql
+++ db/migrations/000002_create_events_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS events
 (
    id                       INTEGER  NOT NULL PRIMARY KEY
-  ,equipment_id             INT  NOT NULL
+  ,equipment_id             VARCHAR(20)  NOT NULL
   ,sighting_date            TIMESTAMP  NOT NULL
   ,sighting_event_code      INTEGER  NOT NULL
   ,reporting_railroad_scac  VARCHAR(4) NOT NULL
   ,posting_date             TIMESTAMP  NOT NULL
-  ,from_mark_id             INT  NOT NULL
+  ,from_mark_id             VARCHAR(5)  NOT NULL
   ,load_empty_status        VARCHAR(1) NOT NULL
   ,sighting_claim_code      VARCHAR(1) NOT NULL
   ,sighting_event_code_text VARCHAR(19) NOT NULL
