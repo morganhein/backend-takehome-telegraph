fa2381d2e0456bdaceffc50d031f94f9a1bbf67c
Morgan Hein <work@morganhe.in>
2022-08-21 16:01:56 -0700

    feat: time scanning for custom type

--- pkg/time/time.go
+++ pkg/time/time.go
@@ -1,35 +1,53 @@
 package time
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 type DateTime struct {
-	time.Time
+	*time.Time
 }
 
 var format = "2006-01-02 15:04:05"
 
-func (date *DateTime) MarshalCSV() (string, error) {
+func (date DateTime) MarshalCSV() (string, error) {
 	return date.Time.Format(format), nil
 }
 
 func (date *DateTime) UnmarshalCSV(csv string) (err error) {
 	if csv == "" {
-		date.Time = time.Time{}
+		date.Time = &time.Time{}
 		return nil
 	}
-	date.Time, err = time.Parse(format, csv)
-	return err
+	t, err := time.Parse(format, csv)
+	if err != nil {
+		return err
+	}
+	date.Time = &t
+	return nil
 }
 
-func (date *DateTime) MarshalJson() (string, error) {
+func (date DateTime) MarshalJson() (string, error) {
 	return date.MarshalJson()
 }
 
 func (date *DateTime) UnmarshalJson(input []byte) (err error) {
 	if len(input) == 0 {
-		date.Time = time.Time{}
+		date.Time = &time.Time{}
 		return nil
 	}
 	err = date.Time.UnmarshalJSON(input)
 	return err
+}
+
+func (date *DateTime) Scan(src any) error {
+	fmt.Println(src)
+	t, ok := src.(time.Time)
+	if !ok {
+		return errors.New("did not recieve a date from db")
+	}
+	date.Time = &t
+	return nil
 }
